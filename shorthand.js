65746b677419de4c353bf564f4bbcc5f0a1b5566
Mounika <mounikachow4@gmail.com>
2017-06-14 11:14:17 -0500

    using short hand props

---
+++ shorthand.js
@@ -0,0 +1,10 @@
+//shorthand properties
+
+let firstName="john";
+let lastName="Lindquist";
+
+let person={firstName, lastName}
+console.log(person);
+let mascot="Moose";
+let team={person,mascot};
+console.log(team);
