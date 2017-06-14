6b2d3ab4889535c53d54bb6c381e56e1f988b24e
Mounika <mounikachow4@gmail.com>
2017-06-14 11:17:55 -0500

    Spread in ES6

---
+++ spread.js
@@ -0,0 +1,16 @@
+//spread operator
+
+let first=[1,2,3];
+let second=[4,5,6];
+first.push(second);
+console.log(first);
+first.push(...second);
+console.log(first);
+
+function addThreeThings(a,b,c){
+	let result=a+b+c;
+	console.log(result);
+}
+addThreeThings(...first);
+addThreeThings(...second);
+
