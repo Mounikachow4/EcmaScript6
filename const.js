af913f3d2e71fd913b3fddfa9c5a0e3202a50554
LAPTOP-NMSQ07ES\Mounika chowdary <dandamudi.mounica@gmail.com>
2017-06-14 11:11:30 -0500

    using const

---
+++ const.js
@@ -0,0 +1,12 @@
+const declarations
+const VALUE="hello world";
+VALUE="foo bar";// VALUE is a read only property
+console.log('value: ', VALUE);
+
+if(true){
+	const foo="bar";
+	console.log('foo: ', foo);
+}
+console.log('foo: ', foo);// foo cannot be accessed outside the loop
+
+
